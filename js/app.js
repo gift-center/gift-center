8494b312dbab2305c04d08734efd7432b0f84cef
izzeldeen01 <alqawasmehizzeldeen@gmail.com>
2021-01-25 19:27:35 +0200

    initial java code

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 // console.log('test'); 
 var sweets;
+var others;
+var budjet;
 function christmas  ( giftBranch, giftSourse) {
     this.gifBranch = giftBranch;
     this.giftSourse = giftSourse;
@@ -38,90 +40,277 @@ function result (event){
     
     // console.log('test submit');
 
-console.log(event);
+// console.log(event);
  sweets = document.getElementById('sweets');
  others = document.getElementById('others');
- budjet = document.getElementById('')
+//  budjet = document.getElementById('budjet');
 
+//  console.log(sweets.checked , others.checked);
+
+if (sweets.checked===true && others.checked===true){
+    for (var i=0 ; i<christmas.prototype.allChristamsGifts.length;i++){
+        
+// display all images in christmas img folder     
+
+    }
+}else if (sweets.checked===false && others.checked===true){
+    for (var i=0 ; i < christmas.prototype.allChristamsGifts.length ; i++){
+        if (christmas.prototype.allChristamsGifts.giftBranch== 'others'){
+
+// display images which are holding others category 
+
+        }
+    }
+}
+else if (sweets.checked===true && others.checked===false ){
+    for (var i=0 ; i < christmas.prototype.allChristamsGifts.length ; i++){
+        if (christmas.prototype.allChristamsGifts.giftBranch== 'sweets'){
+
+      // display images which are holding the sweets category 
+
+    }
+}
+}
+}
+
+
+function Eid  ( giftBranch, giftSourse) {
+    this.gifBranch = giftBranch;
+    this.giftSourse = giftSourse;
+    Eid.prototype.allEidGifts.push(this);
+}
+
+Eid.prototype.allEidGifts = [];
+
+
+new Eid('others', '../assests/eidproducts/img(1).jpeg');
+new Eid('sweets', '../assests/eidproducts/img(1).jpg');
+new Eid('sweets', '../assests/eidproducts/img(1).png');
+new Eid('sweets', '../assests/eidproducts/img(2).jpg');
+new Eid('sweets', '../assests/eidproducts/img(2).png');
+new Eid('sweets', '../assests/eidproducts/img(3).jpg');
+new Eid('sweets', '../assests/eidproducts/img(4).jpg');
+new Eid('sweets', '../assests/eidproducts/img(5).jpg');
+new Eid('sweets', '../assests/eidproducts/img(1).jpg');
+new Eid('others', '../assests/eidproducts/img(7).jpg');
+new Eid('others', '../assests/eidproducts/img(8).jpg');
+new Eid('sweets', '../assests/eidproducts/img(9).jpg');
+new Eid('sweets', '../assests/eidproducts/img(10).jpg');
+new Eid('others', '../assests/eidproducts/img(11).jpg');
+new Eid('others', '../assests/eidproducts/img(12).jpg');
+new Eid('sweets', '../assests/eidproducts/img(13).jpg');
+new Eid('sweets', '../assests/eidproducts/img(14).jpg');
+new Eid('sweets', '../assests/eidproducts/img(15).jpg');
+new Eid('others', '../assests/eidproducts/img(16).jpg');
+
+
+var submit = document.getElementById('sub-form'); 
+
+submit.addEventListener('click',result);
+
+function result (event){
+
+
+    event.preventDefault();
+    
+    // console.log('test submit');
+
+// console.log(event);
+ sweets = document.getElementById('sweets');
+ others = document.getElementById('others');
+//  budjet = document.getElementById('budjet');
+
+//  console.log(sweets.checked , others.checked);
+
+if (sweets.checked===true && others.checked===true){
+    for (var i=0 ; i<Eid.prototype.allEidGifts.length;i++){
+        
+// display all images in christmas img folder     
+
+    }
+}else if (sweets.checked===false && others.checked===true){
+    for (var i=0 ; i <Eid.prototype.allEidGifts.length ; i++){
+        if (Eid.prototype.allEidGifts.giftBranch== 'others'){
+
+// display images which are holding others category 
+
+        }
+    }
+}
+else if (sweets.checked===true && others.checked===false ){
+    for (var i=0 ; i < Eid.prototype.allEidGifts.length ; i++){
+        if (Eid.prototype.allEidGifts.giftBranch== 'sweets'){
+
+      // display images which are holding the sweets category 
+
+    }
+}
+}
 }
 
 
 
+function GetWell  ( giftBranch, giftSourse) {
+    this.gifBranch = giftBranch;
+    this.giftSourse = giftSourse;
+    GetWell.prototype.allGetWellGifts.push(this);
+}
+
+GetWell.prototype.allGetWellGifts = [];
+
+// all GetWell images need to be rebranched 
+
+new GetWell('hotcoca', '../assests/GetWellImg/img1.jpg');
+new GetWell('teddybear', '../assests/GetWellImg/img2.jpg');
+new GetWell('entertatment', '../assests/GetWellImg/img3.jpg');
+new GetWell('entertatment2', '../assests/GetWellImg/img4.jpg');
+new GetWell('cookies', '../assests/GetWellImg/img5.jpg');
+new GetWell('sweets', '../assests/GetWellImg/img6.jpg');
+new GetWell('cookies2', '../assests/GetWellImg/img7.jpg');
+new GetWell('chocolate', '../assests/GetWellImg/img8.jpg');
+new GetWell('chocolate2', '../assests/GetWellImg/img10.jpg');
+new GetWell('fullKit', '../assests/GetWellImg/img11.jpg');
+new GetWell('frutes', '../assests/GetWellImg/img12.jpg');
+new GetWell('toy', '../assests/GetWellImg/img13.jpg');
+new GetWell('skincare', '../assests/GetWellImg/img14.jpg');
+new GetWell('book', '../assests/GetWellImg/img15.jpg');
+new GetWell('showerKit', '../assests/GetWellImg/img16.jpg');
+new GetWell('fullKit2', '../assests/GetWellImg/img17.jpg');
+new GetWell('skincare2', '../assests/GetWellImg/img18.jpg');
+new GetWell('icecream', '../assests/GetWellImg/img19.jpg');
+new GetWell('chocolate3', '../assests/GetWellImg/img20.jpg');
+new GetWell('box', '../assests/GetWellImg/img21.jpg');
+
+
+var submit = document.getElementById('sub-form'); 
+
+submit.addEventListener('click',result);
+
+function result (event){
+
+
+    event.preventDefault();
+    
+    // console.log('test submit');
+
+// console.log(event);
+ sweets = document.getElementById('sweets'); // need to be changed 
+ others = document.getElementById('others');// need to be changed 
+
+// all statments need to be changed OR refactored 
+if (sweets.checked===true && others.checked===true){
+    for (var i=0 ; i<GetWell.prototype.allGetWellGifts.length;i++){
+        
+// display all images in christmas img folder     
+
+    }
+}else if (sweets.checked===false && others.checked===true){
+    for (var i=0 ; i <GetWell.prototype.allGetWellGifts.length ; i++){
+        if (GetWell.prototype.allGetWellGifts.giftBranch== 'others'){
+
+// display images which are holding others category 
+
+        }
+    }
+}
+else if (sweets.checked===true && others.checked===false ){
+    for (var i=0 ; i < Eid.prototype.allEidGifts.length ; i++){
+        if (GetWell.prototype.allGetWellGifts.giftBranch== 'sweets'){
+
+      // display images which are holding the sweets category 
+
+    }
+}
+}
+}
+
+
+function Birthday  ( giftBranch, giftSourse) {
+    this.gifBranch = giftBranch;
+    this.giftSourse = giftSourse;
+    Birthday.prototype.allEidGifts.push(this);
+}
+
+Birthday.prototype.allBirthdayGifts = [];
+
+new Birthday ('accessories', '../assests/birthdayproducts/img(1).jpg');
+new Birthday ('boxes', '../assests/birthdayproducts/img(1).png');
+new Birthday ('sweets', '../assests/birthdayproducts/img(2).jpg');
+new Birthday ('accessories', '../assests/birthdayproducts/img(2).png');
+new Birthday ('accessories', '../assests/birthdayproducts/img(3).jpg');
+new Birthday ('makeup', '../assests/birthdayproducts/img(3).png');
+new Birthday ('boxes', '../assests/birthdayproducts/img(4).jpg');
+new Birthday ('sweets', '../assests/birthdayproducts/img(5).jpg');
+new Birthday ('boxes', '../assests/birthdayproducts/img(6).jpg');
+new Birthday ('accessories', '../assests/birthdayproducts/img(7).jpg');
+new Birthday ('sweets', '../assests/birthdayproducts/img(8).jpg');
+new Birthday ('boxes', '../assests/birthdayproducts/img(9).jpg');
+new Birthday ('accessories', '../assests/birthdayproducts/img(10).jpg');
+new Birthday ('accessories', '../assests/birthdayproducts/img(11).jpg');
+new Birthday ('accessories', '../assests/birthdayproducts/img(12).jpg');
+new Birthday ('sweets', '../assests/birthdayproducts/img(13).jpg');
+new Birthday ('boxes', '../assests/birthdayproducts/img(14).jpg');
+new Birthday ('boxes', '../assests/birthdayproducts/img(15).jpg');
+new Birthday ('boxes', '../assests/birthdayproducts/img(16).jpg');
+new Birthday ('boxes', '../assests/birthdayproducts/img(17).jpg');
+new Birthday ('boxes', '../assests/birthdayproducts/img(18).jpg');
+new Birthday ('sweets', '../assests/birthdayproducts/img(19).jpg');
+new Birthday ('accessories', '../assests/birthdayproducts/img(20).jpg');
+new Birthday ('accessories', '../assests/birthdayproducts/img(21).jpg');
+new Birthday ('others', '../assests/birthdayproducts/img(22).jpg');
+new Birthday ('accessories', '../assests/birthdayproducts/img(23).jpg');
+new Birthday ('sweets', '../assests/birthdayproducts/img(24).jpg');
+new Birthday ('sweets', '../assests/birthdayproducts/img(25).jpg');
+new Birthday ('boxes', '../assests/birthdayproducts/img(26).jpg');
+new Birthday ('boxes', '../assests/birthdayproducts/img(27).jpg');
+new Birthday ('others', '../assests/birthdayproducts/img(28).jpg');
+new Birthday ('others', '../assests/birthdayproducts/img(29).jpg');
+new Birthday ('sweets', '../assests/birthdayproducts/img(30).jpg');
+new Birthday ('sweets', '../assests/birthdayproducts/img(31).jpg');
 
 
 
+var submit = document.getElementById('sub-form'); 
+
+submit.addEventListener('click',result);
+
+function result (event){
 
 
+    event.preventDefault();
+    
+    // console.log('test submit');
 
+// console.log(event);
+ sweets = document.getElementById('sweets'); // need to be changed 
+ others = document.getElementById('others');// need to be changed 
 
+// all statments need to be changed OR refactored 
+if (sweets.checked===true && others.checked===true){
+    for (var i=0 ; i<GetWell.prototype.allGetWellGifts.length;i++){
+        
+// display all images in christmas img folder     
 
+    }
+}else if (sweets.checked===false && others.checked===true){
+    for (var i=0 ; i <GetWell.prototype.allGetWellGifts.length ; i++){
+        if (GetWell.prototype.allGetWellGifts.giftBranch== 'others'){
 
+// display images which are holding others category 
 
+        }
+    }
+}
+else if (sweets.checked===true && others.checked===false ){
+    for (var i=0 ; i < Eid.prototype.allEidGifts.length ; i++){
+        if (GetWell.prototype.allGetWellGifts.giftBranch== 'sweets'){
 
+      // display images which are holding the sweets category 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// new gift('eid', 'coffee', '../assests/eidproducts/img(1).jpeg');
-// new gift('eid', 'sweet', '../assests/eidproducts/img(1).jpg');
-// new gift('eid', 'sweet2', '../assests/eidproducts/img(1).png');
-// new gift('eid', 'chocolate', '../assests/eidproducts/img(2).jpg');
-// new gift('eid', 'sweet3', '../assests/eidproducts/img(2).png');
-// new gift('eid', 'sweet4', '../assests/eidproducts/img(3).jpg');
-// new gift('eid', 'sweet5', '../assests/eidproducts/img(4).jpg');
-// new gift('eid', 'sweet6', '../assests/eidproducts/img(5).jpg');
-// new gift('eid', 'sweet7', '../assests/eidproducts/img(1).jpg');
-// new gift('eid', 'coffee-contaner', '../assests/eidproducts/img(7).jpg');
-// new gift('eid', 'coffee-contaner2', '../assests/eidproducts/img(8).jpg');
-// new gift('eid', 'sweet8', '../assests/eidproducts/img(9).jpg');
-// new gift('eid', 'sweet9', '../assests/eidproducts/img(10).jpg');
-// new gift('eid', 'box', '../assests/eidproducts/img(11).jpg');
-// new gift('eid', 'box2', '../assests/eidproducts/img(12).jpg');
-// new gift('eid', 'sweet10', '../assests/eidproducts/img(13).jpg');
-// new gift('eid', 'sweet11', '../assests/eidproducts/img(14).jpg');
-// new gift('eid', 'sweet12', '../assests/eidproducts/img(15).jpg');
-// new gift('eid', 'coffee-contaner3', '../assests/eidproducts/img(16).jpg');
-
-// new gift('getwell', 'hotcoca', '../assests/GetWellImg/img1.jpg');
-// new gift('getwell', 'teddybear', '../assests/GetWellImg/img2.jpg');
-// new gift('getwell', 'entertatment', '../assests/GetWellImg/img3.jpg');
-// new gift('getwell', 'entertatment2', '../assests/GetWellImg/img4.jpg');
-// new gift('getwell', 'cookies', '../assests/GetWellImg/img5.jpg');
-// new gift('getwell', 'sweets', '../assests/GetWellImg/img6.jpg');
-// new gift('getwell', 'cookies2', '../assests/GetWellImg/img7.jpg');
-// new gift('getwell', 'chocolate', '../assests/GetWellImg/img8.jpg');
-// new gift('getwell', 'chocolate2', '../assests/GetWellImg/img10.jpg');
-// new gift('getwell', 'fullKit', '../assests/GetWellImg/img11.jpg');
-// new gift('getwell', 'frutes', '../assests/GetWellImg/img12.jpg');
-// new gift('getwell', 'toy', '../assests/GetWellImg/img13.jpg');
-// new gift('getwell', 'skincare', '../assests/GetWellImg/img14.jpg');
-// new gift('getwell', 'book', '../assests/GetWellImg/img15.jpg');
-// new gift('getwell', 'showerKit', '../assests/GetWellImg/img16.jpg');
-// new gift('getwell', 'fullKit2', '../assests/GetWellImg/img17.jpg');
-// new gift('getwell', 'skincare2', '../assests/GetWellImg/img18.jpg');
-// new gift('getwell', 'icecream', '../assests/GetWellImg/img19.jpg');
-// new gift('getwell', 'chocolate3', '../assests/GetWellImg/img20.jpg');
-// new gift('getwell', 'box', '../assests/GetWellImg/img21.jpg');
-
-
-
-
-console.log(christmas.prototype.allChristamsGifts);
-
-
+    }
+}
+}
+}
 
 
 
