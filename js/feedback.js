cd57d98f946484f0258888ba5a9dab9b27bb3251
farhanayyash <farhanayyash16@yahoo.com>
2021-01-25 12:13:14 +0200

    fix com

--- js/feedback.js
+++ js/feedback.js
@@ -16,7 +16,21 @@ document.getElementById("submitBtn").addEventListener("click", function(event)
   }
   var d = new Date();
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var fullcom = name +":   "+ namef +"              at "+ d.getFullYear()+"/"+months[d.getMonth()]+"/"+d.getDate();
+
+  var fullname= name;
+  var fulldate ="at: " + d.getFullYear()+"/"+months[d.getMonth()]+"/"+d.getDate()
+  var fullcom = namef ;
+
+  var existing1 = localStorage.getItem('name');
+  existing1 = existing1 ? existing1.split(',') : [];
+  existing1.push(fullname);
+  localStorage.setItem('name', existing1.toString());
+
+  var existing2 = localStorage.getItem('date');
+  existing2 = existing2 ? existing2.split(',') : [];
+  existing2.push(fulldate);
+  localStorage.setItem('date', existing2.toString());
+
   var existing = localStorage.getItem('lastcomment');
   existing = existing ? existing.split(',') : [];
   existing.push(fullcom);
@@ -27,29 +41,42 @@ document.getElementById("submitBtn").addEventListener("click", function(event)
 
 
 if(localStorage.getItem("lastcomment")){
-  var existing = localStorage.getItem('lastcomment');
-  existing = existing ? existing.split(',') : [];
-  var writeRow = document.getElementById("comfeld");
-  for (var i =0; i < existing.length ; i++){
-    var interestLast = document.createElement('li');
-    interestLast.textContent = existing[i];
-    interestLast.setAttribute("class","commit");
-    writeRow.appendChild(interestLast);
-    var interestLast2 = document.createElement('br');
-    writeRow.appendChild(interestLast2);
-
-  }
+  render();
   
 }
 
 function render(){
   var existing = localStorage.getItem('lastcomment');
+  var existing2 = localStorage.getItem('name');
+  var existing3 = localStorage.getItem('date');
+
   existing = existing ? existing.split(',') : [];
+  existing2 = existing2 ? existing2.split(',') : [];
+  existing3 = existing3 ? existing3.split(',') : [];
+
+
+  console.log(existing);
+  console.log(existing2);
+  console.log(existing3);
+
   var writeRow = document.getElementById("comfeld");
+
   writeRow.innerHTML="";
+
   for (var i =0; i < existing.length ; i++){
     var interestLast = document.createElement('li');
-    interestLast.textContent = existing[i];
+    var icreat = document.createElement('i');
+    icreat.textContent= existing2[i];
+    interestLast.appendChild(icreat);
+    var pcreate = document.createElement('p');
+    pcreate.textContent= existing[i];
+    interestLast.appendChild(pcreate);
+    var spmcreate = document.createElement('samp');
+    spmcreate.textContent= existing3[i];
+    interestLast.appendChild(spmcreate);
+
+
+    
     writeRow.appendChild(interestLast);
     var interestLast2 = document.createElement('br');
     writeRow.appendChild(interestLast2);
