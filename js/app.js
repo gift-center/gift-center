fe2e02a65ffb81c309f05e5b0f7bc20d3aeb54d5
samehmasri <sameh.s.almasri@gmail.com>
2021-01-25 19:12:22 +0200

    JavaObjects

--- js/app.js
+++ js/app.js
@@ -1,57 +1,225 @@
-// 'use strict';
+'use strict' ; 
 
-// var imageArray=[];
+function MotherDay (giftBranch,giftSourse){
+    this.giftBranch = giftBranch; 
+    this.giftSourse = giftSourse;
+    MotherDay.prototype.allMotherDayGifts.push(this); 
+}
 
-// var divCards= document.getElementById('main-img');
-// var cardDiv = document.getElementsById('card');
-// var cardContainer = document.getElementsById('container');
+MotherDay.prototype.allMotherDayGifts = []; 
 
-// //divCards.appendChild(cardDiv);
+new MotherDay ('figure','../assets/MothersDayImg/img1.jpg');
+new MotherDay ('box','../assets/MothersDayImg/img2.jpg');
+new MotherDay ('box','../assets/MothersDayImg/img3.jpg');
+new MotherDay ('decore','../assets/MothersDayImg/img4.jpg');
+new MotherDay ('decore','../assets/MothersDayImg/img5.jpg');
+new MotherDay ('box','../assets/MothersDayImg/img6.jpg');
+new MotherDay ('decore','../assets/MothersDayImg/img8.jpg');
+new MotherDay ('accessory','../assets/MothersDayImg/img9.jpg');
+new MotherDay ('box','../assets/MothersDayImg/img10.jpg');
+new MotherDay ('clothes','../assets/MothersDayImg/img11.jpg');
+new MotherDay ('citchen','../assets/MothersDayImg/img12.jpg');
+new MotherDay ('box','../assets/MothersDayImg/img13.jpg');
+new MotherDay ('rose','../assets/MothersDayImg/img14.jpg');
+new MotherDay ('citchen','../assets/MothersDayImg/img15.jpg');
+new MotherDay ('decore','../assets/MothersDayImg/img16.jpg');
 
-// function ImageCard(nameOfOccasion, sourceOfImage){
-//     this.nameOfOccasion= nameOfOccasion;
-//     this.sourceOfImage = sourceOfImage;
-//     this.clicked = 0;
+function NewPorn (giftBranch,giftSourse){
+    this.giftBranch = giftBranch; 
+    this.giftSourse = giftSourse;
+    NewPorn.prototype.allNewPornGifts.push(this); 
+}
 
-//     imageArray.push(this);
-// }
+NewPorn.prototype.allNewPornGifts = []; 
 
-// var aniversaryCard = new ImageCard('Aniversary Gifts', 'assests/occassions/aniversary.jpeg');
-// var birthdayCard = new ImageCard('Birthday Gifts', 'assests/occassions/birthday.jpeg');
-// var christmasCard = new ImageCard('Christmas Gifts', 'assests/occassions/christmas.jpeg');
-// var congratulationsCard = new ImageCard('Congratulations Gifts', 'assests/occassions/congratulations.jpeg');
-// var eidCard = new ImageCard('Eid Gifts', 'assests/occassions/eid.jpeg');
-// var getwellCard = new ImageCard('Get Well Gifts', 'assests/occassions/getwell.jpeg');
-// var graduationCard = new ImageCard('Graduation Gifts', 'assests/occassions/graduation.jpeg');
-// var mothersdayCard = new ImageCard('Mothers Day Gifts', 'assests/occassions/mothersday.jpeg');
-// var newbornCard = new ImageCard('New Born Gifts', 'assests/occassions/newborn.jpeg');
-// var newhouseCard = new ImageCard('New House Gifts', 'assests/occassions/newhouse.jpeg');
-// var newyearCard = new ImageCard('New Year Gifts', 'assests/occassions/newyear.jpeg');
-// var ramadanCard = new ImageCard('Ramadan Gifts', 'assests/occassions/ramadan.jpeg');
-// var thankyouCard = new ImageCard('Thank You Gifts', 'assests/occassions/thankyou.jpeg');
-// var valentinesdayCard = new ImageCard('Valentine\'s Day Gifts', 'assests/occassions/valentinesday.jpeg');
-// var weddingCard = new ImageCard('Wedding Gifts', 'assests/occassions/wedding.jpeg');
+new NewPorn ('carriage','../assets/NewBornImg/img1.webp');
+new NewPorn ('clothes','../assets/NewBornImg/img2.jpg');
+new NewPorn ('box','../assets/NewBornImg/img3.jpg');
+new NewPorn ('box','../assets/NewBornImg/img4.jpg');
+new NewPorn ('box','../assets/NewBornImg/img5.jpg');
+new NewPorn ('clothes','../assets/NewBornImg/img6.jpg');
+new NewPorn ('clothes','../assets/NewBornImg/img7.jpg');
+new NewPorn ('box','../assets/NewBornImg/img8.jpg');
+new NewPorn ('clothes','../assets/NewBornImg/img9.jpg');
+new NewPorn ('clothes','../assets/NewBornImg/img10.jpg');
+new NewPorn ('clothes','../assets/NewBornImg/img11.jpg');
+new NewPorn ('clothes','../assets/NewBornImg/img12.jpg');
+new NewPorn ('clothes','../assets/NewBornImg/img13.webp');
+new NewPorn ('clothes','../assets/NewBornImg/img14.jpg');
+new NewPorn ('box','../assets/NewBornImg/img2.jpeg');
+new NewPorn ('clothes','../assets/NewBornImg/img16.jpg');
+new NewPorn ('clothes','../assets/NewBornImg/img17.jpg');
 
-// renderCards();
+function Graduation (giftBranch,giftSourse){
+    this.giftBranch = giftBranch; 
+    this.giftSourse = giftSourse;
+    Graduation.prototype.allGraduationGifts.push(this); 
+}
 
-// function renderCards(){
+Graduation.prototype.allGraduationGifts = []; 
 
-//     for(var i=0; i<imageArray.length; i++){
-//         // Card Div
-//         var cardImage = document.createElement('img');
-//         cardDiv.appendChild(cardImage);
-//         cardImage.src= imageArray[i].sourceOfImage;
+new Graduation ('hangings','../assets/graduations products/img-1.jpg');
+new Graduation ('cups','../assets/graduations products/img-2.jpg');
+new Graduation ('cups','../assets/graduations products/img-3.png');
+new Graduation ('hangings','../assets/graduations products/img-4.jpg');
+new Graduation ('cups','../assets/graduations products/img-5.png');
+new Graduation ('decore','../assets/graduations products/img-6.jpg');
+new Graduation ('hangings','../assets/graduations products/img-7.jpg');
+new Graduation ('box','../assets/graduations products/img-8.jpg');
+new Graduation ('figure','../assets/graduations products/img-9.jpg');
+new Graduation ('figure','../assets/graduations products/img-10.jpg');
+new Graduation ('hangings','../assets/graduations products/img-11.jpg');
+new Graduation ('hangings','../assets/graduations products/img-12.jpg');
+new Graduation ('hangings','../assets/graduations products/img-13.jpg');
+new Graduation ('sweet','../assets/graduations products/img-14.jpg');
+new Graduation ('sweet','../assets/graduations products/img-15.jpg');
+new Graduation ('cups','../assets/graduations products/img-16.jpg');
+new Graduation ('sweet','../assets/graduations products/img-17.jpg');
+new Graduation ('box','../assets/graduations products/img-18.jpg');
+new Graduation ('decore','../assets/graduations products/img-19.jpg');
+new Graduation ('decore','../assets/graduations products/img-20.jpg');
 
-//         var cardTitle = document.createElement('h4');
-//         var cardLink = document.createElement('a');
 
-//         cardContainer.appendChild(cardTitle);
-//         cardContainer.appendChild(cardLink);
+function NewHome (giftBranch,giftSourse){
+    this.giftBranch = giftBranch; 
+    this.giftSourse = giftSourse;
+    NewHome.prototype.allNewHomeGifts.push(this); 
+}
 
-//         cardTitle.textContent = imageArray[i].nameOfOccasion;
-//         cardLink.textContent = 'Order';
+NewHome.prototype.allNewHomeGifts = []; 
 
-//     }
+new NewHome ('hangings','../assets/homeImage/img-1.jpg');
+new NewHome ('hangings','../assets/homeImage/img-2.jpg');
+new NewHome ('hangings','../assets/homeImage/img-3.jpg');
+new NewHome ('hangings','../assets/homeImage/img-4.jpg');
+new NewHome ('hangings','../assets/homeImage/img-5.jpg');
+new NewHome ('hangings','../assets/homeImage/img-6.jpg');
+new NewHome ('hangings','../assets/homeImage/img-7.jpg');
+new NewHome ('hangings','../assets/homeImage/img-8.jpg');
+new NewHome ('hangings','../assets/homeImage/img-9.jpg');
+new NewHome ('hangings','../assets/homeImage/img-10.jpg');
+new NewHome ('hangings','../assets/homeImage/img-11.jpg');
+new NewHome ('hangings','../assets/homeImage/img-12.jpg');
+new NewHome ('hangings','../assets/homeImage/img-13.jpg');
+new NewHome ('hangings','../assets/homeImage/img-14.jpg');
+new NewHome ('hangings','../assets/homeImage/img-15.jpg');
+new NewHome ('hangings','../assets/homeImage/img-16.jpg');
+new NewHome ('hangings','../assets/homeImage/img-17.jpg');
 
 
-// }
+function NewYear (giftBranch,giftSourse){
+    this.giftBranch = giftBranch; 
+    this.giftSourse = giftSourse;
+    NewYear.prototype.allNewYearGifts.push(this); 
+}
+
+NewYear.prototype.allNewYearGifts = []; 
+
+new NewYear ('sweetBox','../assets/NewYearIMG/img 1.jpg');
+new NewYear ('giftBox','../assets/NewYearIMG/img 2');
+new NewYear ('sweetBox','../assets/NewYearIMG/img 3.jpg');
+new NewYear ('sweetBox','../assets/NewYearIMG/img 4.jpg');
+new NewYear ('sweetBox','../assets/NewYearIMG/img 5.jpg');
+new NewYear ('sweetBox','../assets/NewYearIMG/img 6.jpg');
+new NewYear ('giftBox','../assets/NewYearIMG/img 7.jpg');
+new NewYear ('card','../assets/NewYearIMG/img 8.jpg');
+new NewYear ('giftBox','../assets/NewYearIMG/img 9.jpg');
+new NewYear ('giftBox','../assets/NewYearIMG/img 10.jpg');
+new NewYear ('giftBox','../assets/NewYearIMG/img 11.jpg');
+new NewYear ('rose','../assets/NewYearIMG/img 12.jpg');
+new NewYear ('giftBox','../assets/NewYearIMG/img 13.jpg');
+new NewYear ('sweetBox','../assets/NewYearIMG/img 14.jpg');
+new NewYear ('giftBox','../assets/NewYearIMG/img 15.jpg');
+new NewYear ('sweetBox','../assets/NewYearIMG/img 16.jpg');
+new NewYear ('sweetBox','../assets/NewYearIMG/img 17.jpg');
+
+
+function Ramadan (giftBranch,giftSourse){
+    this.giftBranch = giftBranch; 
+    this.giftSourse = giftSourse;
+    Ramadan.prototype.allRamadanGifts.push(this); 
+}
+
+Ramadan.prototype.allRamadanGifts = []; 
+
+new Ramadan ('hangings','../assets/RamadanImage/img-1.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-2.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-3.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-4.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-5.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-6.jpg');
+new Ramadan ('dalleh&tammer','../assets/RamadanImage/img-7.jpg');
+new Ramadan ('dalleh&tammer','../assets/RamadanImage/img-8.jpg');
+new Ramadan ('dalleh&tammer','../assets/RamadanImage/img-9.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-10.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-11.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-12.png');
+new Ramadan ('hangings','../assets/RamadanImage/img-13.jpg');
+new Ramadan ('dalleh&tammer','../assets/RamadanImage/img-14.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-15.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-16.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-17.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-18.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-19.jpg');
+new Ramadan ('hangings','../assets/RamadanImage/img-20.jpg');
+
+
+function Valentine (giftBranch,giftSourse){
+    this.giftBranch = giftBranch; 
+    this.giftSourse = giftSourse;
+    Valentine.prototype.allValentineGifts.push(this); 
+}
+
+Valentine.prototype.allValentineGifts = []; 
+
+new Valentine ('box','../assets/valentineIMG/img 1.jpg');
+new Valentine ('box','../assets/valentineIMG/img 2.jpg');
+new Valentine ('sweet','../assets/valentineIMG/img 3.jpg');
+new Valentine ('figure','../assets/valentineIMG/img 4.jpg');
+new Valentine ('figure','../assets/valentineIMG/img 5.jpg');
+new Valentine ('gift','../assets/valentineIMG/img 6.jpg');
+new Valentine ('rose','../assets/valentineIMG/img 7.jpg');
+new Valentine ('sweet','../assets/valentineIMG/img 8.jpg');
+new Valentine ('box','../assets/valentineIMG/img 9.jpg');
+new Valentine ('box','../assets/valentineIMG/img 10.jpg');
+new Valentine ('sweet','../assets/valentineIMG/img 11.jpg');
+new Valentine ('gift','../assets/valentineIMG/img 12.jpg');
+new Valentine ('sweet','../assets/valentineIMG/img 13.jpg');
+new Valentine ('gift','../assets/valentineIMG/img 14.jpg');
+new Valentine ('sweet','../assets/valentineIMG/img 15.jpg');
+new Valentine ('sweet','../assets/valentineIMG/img 16.jpg');
+new Valentine ('sweet','../assets/valentineIMG/img 17.jpg');
+new Valentine ('gift','../assets/valentineIMG/img 18.jpg');
+new Valentine ('gift','../assets/valentineIMG/img 19.jpg');
+new Valentine ('gift','../assets/valentineIMG/img 20.jpg');
+new Valentine ('box','../assets/valentineIMG/img 21.jpg');
+
+
+function Wedding (giftBranch,giftSourse){
+    this.giftBranch = giftBranch; 
+    this.giftSourse = giftSourse;
+    Wedding.prototype.allWeddingGifts.push(this); 
+}
+
+Wedding.prototype.allWeddingGifts = []; 
+
+new Wedding ('box','../assets/weddingIMG/img 1.jpg');
+new Wedding ('box','../assets/weddingIMG/img 2.jpg');
+new Wedding ('box','../assets/weddingIMG/img 3.jpg');
+new Wedding ('wood','../assets/weddingIMG/img 4.jpg');
+new Wedding ('box','../assets/weddingIMG/img 5.jpg');
+new Wedding ('wood','../assets/weddingIMG/img 6.jpg');
+new Wedding ('box','../assets/weddingIMG/img 7.jpg');
+new Wedding ('box','../assets/weddingIMG/img 8.png');
+new Wedding ('wood','../assets/weddingIMG/img 9.jpg');
+new Wedding ('box','../assets/weddingIMG/img 10.jpg');
+new Wedding ('wood','../assets/weddingIMG/img 11.jpg');
+new Wedding ('perfume','../assets/weddingIMG/img 12.jpg');
+new Wedding ('box','../assets/weddingIMG/img 13.jpg');
+new Wedding ('wood','../assets/weddingIMG/img 14.jpg');
+new Wedding ('plates','../assets/weddingIMG/img 15.jpg');
+new Wedding ('box','../assets/weddingIMG/img 16.jpg');
+new Wedding ('box','../assets/weddingIMG/img 17.jpg');
+new Wedding ('box','../assets/weddingIMG/img 18.jpeg');
+
+
